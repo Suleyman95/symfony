13d12ddd8c76ae59d472e7027421d56bd34044b4
msv <youmsv@gmail.com>
2018-05-05 00:51:13 +0300

    commit parsing

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: msv
+ * Date: 04.05.18
+ * Time: 20:01
+ */
+
+namespace App\Command;
+
+use App\Entity\NamespaceSymfony;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class CreateUserCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            // the name of the command (the part after "bin/console")
+            ->setName('parse:symfony')
+
+            // the short description shown while running "php bin/console list"
+            ->setDescription('Creates a new user.')
+
+            // the full command description shown when running the command with
+            // the "--help" option
+            ->setHelp('This command allows you to create a user...')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $html = file_get_contents("https://api.symfony.com/4.0/");
+        $crawler = new Crawler($html);
+
+        $crawler = $crawler->filter('.namespace-container > ul > li');
+
+        $em = $this->getContainer()->get('doctrine.orm.default_entity_manager');
+
+        foreach ($crawler as $element) {
+            $namespace = new NamespaceSymfony();
+
+            $namespace->setName($element->textContent);
+
+            $em->persist($namespace);
+        }
+
+        $em->flush();
+    }
+}
